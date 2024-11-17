939830855b04e088e6088865e43fa8bd6b5cf23b
Abdwarwar <abdelsalam.warwar@hotmail.com>
2024-11-17 09:40:16 +0300

    widget.js

--- widget.js
+++ widget.js
@@ -1,8 +1,8 @@
 (function () {
   const template = document.createElement("template");
   template.innerHTML = `
-    <div class="hello-world-container">
-      <h1></h1>
+    <div>
+      <h1 id="helloText">Hello, World!</h1>
     </div>
   `;
 
@@ -11,38 +11,11 @@
       super();
       this.attachShadow({ mode: "open" });
       this.shadowRoot.appendChild(template.content.cloneNode(true));
-      this._text = "Hello World"; // Default text
     }
 
     connectedCallback() {
-      this._updateText();
-    }
-
-    // Define a property to bind text
-    static get observedAttributes() {
-      return ["text"];
-    }
-
-    attributeChangedCallback(name, oldValue, newValue) {
-      if (name === "text") {
-        this._text = newValue;
-        this._updateText();
-      }
-    }
-
-    _updateText() {
-      const h1 = this.shadowRoot.querySelector("h1");
-      h1.textContent = this._text;
-    }
-
-    // Example method
-    init() {
-      console.log("Widget initialized!");
-    }
-
-    // Event trigger (click event)
-    _onClick() {
-      this.dispatchEvent(new Event("onClick"));
+      const helloText = this.shadowRoot.getElementById("helloText");
+      helloText.textContent = "Hello, Custom Widget!";
     }
   }
 
