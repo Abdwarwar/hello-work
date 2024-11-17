b9d7aac9d2b000161adbe269b46e6a330505691a
Abdwarwar <abdelsalam.warwar@hotmail.com>
2024-11-17 09:03:59 +0300

    Update widget.js

--- widget.js
+++ widget.js
@@ -1,18 +1,50 @@
 (function () {
-    const template = document.createElement("template");
-    template.innerHTML = `
-        <div class="hello-world-container">
-            <h1>Hello, World!</h1>
-        </div>
-    `;
+  const template = document.createElement("template");
+  template.innerHTML = `
+    <div class="hello-world-container">
+      <h1></h1>
+    </div>
+  `;
 
-    class HelloWorldWidget extends HTMLElement {
-        constructor() {
-            super();
-            const shadow = this.attachShadow({ mode: "open" });
-            shadow.appendChild(template.content.cloneNode(true));
-        }
+  class HelloWorldWidget extends HTMLElement {
+    constructor() {
+      super();
+      this.attachShadow({ mode: "open" });
+      this.shadowRoot.appendChild(template.content.cloneNode(true));
+      this._text = "Hello World"; // Default text
     }
 
-    customElements.define("com-sap-custom-helloworld", HelloWorldWidget);
+    connectedCallback() {
+      this._updateText();
+    }
+
+    // Define a property to bind text
+    static get observedAttributes() {
+      return ["text"];
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+      if (name === "text") {
+        this._text = newValue;
+        this._updateText();
+      }
+    }
+
+    _updateText() {
+      const h1 = this.shadowRoot.querySelector("h1");
+      h1.textContent = this._text;
+    }
+
+    // Example method
+    init() {
+      console.log("Widget initialized!");
+    }
+
+    // Event trigger (click event)
+    _onClick() {
+      this.dispatchEvent(new Event("onClick"));
+    }
+  }
+
+  customElements.define("com-sap-custom-helloworld", HelloWorldWidget);
 })();
